4a972c9d2065f9fc30336ad986059e97837b1630
David Tilson <davidrtilson@gmail.com>
2020-04-22 15:28:02 +0100

    finished work on reading csv file and printint the tasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 public class Gantt extends PApplet
 {	
+	ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	public void settings()
 	{
@@ -19,11 +20,23 @@ public class Gantt extends PApplet
 		Table t = loadTable("tasks.csv", "header");
 		for (TableRow tr : t.rows())
 		{
-			
+			Task task = new Task(tr);
+			tasks.add(task);
 		}
 	}
 
 	public void printTasks()
+	{
+		println("Printing Tasks:");
+
+		for (Task task : tasks)
+		{
+			println(task.toString());
+			//println(task.getPhase() + '\t' + task.getStart() + '\t' + task.getEnd() + '\n');
+		}
+	}
+
+	public void displayTasks()
 	{
 		
 	}
@@ -37,16 +50,18 @@ public class Gantt extends PApplet
 	{
 		println("Mouse dragged");
 	}
-
-	//
-	
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
-	{			
+	{	
 		background(0);
+
+		//start of writing names
+		
 	}
 }
--- java/src/ie/tudublin/Task.java
+++ java/src/ie/tudublin/Task.java
@@ -6,7 +6,61 @@ import processing.data.Table;
 import processing.data.TableRow;
 import java.util.ArrayList;
 
-public class Task extends PApplet
+public class Task extends Gantt
 {
-    
+    private String phase;
+    private int start;
+    private int end;
+
+    public Task(String phase, int start, int end)
+    {
+        this.phase = phase;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Task(TableRow tr)
+    {
+        this(
+            tr.getString("Task"),
+            tr.getInt("Start"),
+            tr.getInt("End")
+        );
+    }
+
+    //Getters and Setters
+    public void setPhase(String phase)
+    {
+        this.phase = phase;
+    }
+
+    public String getPhase()
+    {
+        return phase;
+    }
+
+    public void setStart(int start)
+    {
+        this.start = start;
+    }
+
+    public int getStart()
+    {
+        return start;
+    }
+
+    public void setEnd(int end)
+    {
+        this.end = end;
+    }
+
+    public int getEnd()
+    {
+        return end;
+    }
+
+    public String toString()
+    {
+        return phase + "  " + start + "  " + end + "  ";
+    }
 }
